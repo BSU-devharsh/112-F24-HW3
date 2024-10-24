615c59a9f03e08b8e6c02f5caa64c35d4920830b
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-10-24 15:27:22 -0400

    Create FactorialUsingDoWhile.java

---
+++ FactorialUsingDoWhile.java
@@ -0,0 +1,18 @@
+// Write your name here
+
+// 2. Factorial using do-while loop
+
+import java.util.Scanner;
+
+public class FactorialUsingDoWhile {
+    public static long calculateFactorial(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        System.out.println("Factorial of " + number + " is: " + calculateFactorial(number));
+    }
+}
