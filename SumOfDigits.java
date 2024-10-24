38184024e4dc6f148f5f83561ba6f32fb4090653
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-10-24 15:31:19 -0400

    Create SumOfDigits.java

---
+++ SumOfDigits.java
@@ -0,0 +1,18 @@
+// Write your name here
+
+// 5. Sum of digits
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static int calculateDigitSum(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        System.out.println("Sum of digits of " + number + " is: " + calculateDigitSum(number));
+    }
+}
