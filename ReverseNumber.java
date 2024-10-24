73332e08687490a209e9c17a934b3b0a5dba5684
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-10-24 15:29:16 -0400

    Create ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,18 @@
+// Write your name here
+
+// 4. Reverse digits of a number
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static int reverseNumber(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        System.out.println("Reversed number of " + number + " is: " + reverseNumber(number));
+    }
+}
